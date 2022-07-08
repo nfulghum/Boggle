de144dd23231c3bc021dad66f1c8f6fe04b175b9
Nick Fulghum <nickfulghum92@gmail.com>
2022-07-08 16:04:14 -0500

    Testing completed

--- test.py
+++ test.py
@@ -10,3 +10,48 @@ class FlaskTests(TestCase):
         """do before every test"""
         self.client = app.test_client()
         app.config['TESTING'] = True
+
+    def test_game_board(self):
+        with self.client:
+            res = self.client.get('/')
+            html = res.get_data(as_text=True)
+
+            self.assertIn('Score: 0', html)
+            self.assertIn('Times Played: 0', html)
+            self.assertIn('High Score: 0', html)
+            self.assertIn('board', session)
+            self.assertIsNone(session.get('highscore'))
+            self.assertIsNone(session.get('numplays'))
+
+    def test_valid_word(self):
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [["T", "A", "T", "T", "T"],
+                                 ["C", "R", "C", "K", "T"],
+                                 ["C", "U", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"]]
+        response = self.client.get('/check-word?word=truck')
+        self.assertEqual(response.json['response'], 'ok')
+
+    def test_not_on_board(self):
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [["T", "A", "T", "T", "T"],
+                                 ["C", "R", "C", "K", "T"],
+                                 ["C", "U", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"]]
+        response = self.client.get('/check-word?word=board')
+        self.assertEqual(response.json['response'], 'not-on-board')
+
+    def test_not_word(self):
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [["T", "A", "T", "T", "T"],
+                                 ["C", "R", "C", "K", "T"],
+                                 ["C", "U", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"]]
+        response = self.client.get('/check-word?word=tasdfsadf')
+        self.assertEqual(response.json['response'], 'not-word')
